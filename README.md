3412f1af427388a6b7c8ddb892a6e61eb583a3da
Navneet Gupta <navneet.gupta@reedbusiness.com>
2021-02-24 02:06:16 +0530

    added timestamp check, updated readme, extra input validation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# assignments
+# Assignment
+
+*Source file assumptions*
+- already present data could be edited
+- new row can be added anywhere (start, middle, end)
+- lines with just white spaces could be present
+
+*Logic for reading the file*
+1. Last modified date would be used to decide whether to read the file or not.
+2. Initially, last modified date would be set as 1980 (to ensure that file is read at least once)
+3. If a file is modified, it's last modified date would be updated, this passes the test for reading the file next time
--- src/com/flight/system/DataReader.java
+++ src/com/flight/system/DataReader.java
@@ -1,13 +1,12 @@
 package com.flight.system;
 
-import javax.security.auth.login.Configuration;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
@@ -22,11 +21,17 @@ public class DataReader implements Runnable {
     private final static String fileName = ConfigurationLoader.getPropertyValue("sourceFile");
     Flights flights;
     List<List<String>> result;
-    static Handler fileHandler = null;
+    Handler fileHandler = null;
+    public long lastModifiedTime = Long.parseLong(ConfigurationLoader.getPropertyValue("timeInMs1980"));//315513000000L; //01-01-1980
     private static final Logger LOGGER = Logger.getLogger(ReadScheduler.class
             .getClass().getName());
 
-    public static void setup() {
+    public long getLastModifiedTime() throws IOException {
+        BasicFileAttributes attr = Files.readAttributes(Paths.get(fileName), BasicFileAttributes.class);
+        return attr.lastModifiedTime().toMillis();
+    }
+
+    public void setup() {
         try {
             fileHandler = new FileHandler("logs/logfile.log");
             SimpleFormatter simple = new SimpleFormatter();
@@ -43,12 +48,17 @@ public class DataReader implements Runnable {
     }
 
     public synchronized void readData(Flights flights) throws IOException {
-        LOGGER.info("Reading...");
-        result = Files.readAllLines(Paths.get(fileName))
-                .stream()
-                .map(line -> Arrays.asList(line.split(PIPE_DELIMITER)))
-                .collect(Collectors.toList());
-        makeObject(result, flights);
+        if (lastModifiedTime < getLastModifiedTime()) {
+            LOGGER.info("Reading...");
+            System.out.println("Reading...");
+            lastModifiedTime = getLastModifiedTime();
+            result = Files.readAllLines(Paths.get(fileName))
+                    .stream()
+                    .filter(line -> !line.trim().isEmpty()) // ignoring empty lines
+                    .map(line -> Arrays.asList(line.split(PIPE_DELIMITER)))
+                    .collect(Collectors.toList());
+            makeObject(result, flights);
+        }
     }
 
     public synchronized List<List<String>> getResult() {
@@ -66,7 +76,7 @@ public class DataReader implements Runnable {
                     }
                     return null;
                 })
-                .forEach(flightRecord -> flights.addFlight(flightRecord));
+                .forEach(flights::addFlight);
     }
 
     public Flight mapper(List<String> data) throws ParseException {
